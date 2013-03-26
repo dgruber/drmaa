71fcac0ddda61a59bdbda75e2dcda31556d31d4a
dgruber <mail@danielgruber.com>
2013-03-26 17:10:41 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # You need to source the settings.sh file (source /path/to/GE/default/common/settings.sh)
 # of your Grid Engine installation before building. "default" is the CELL name, which can
-# be different in the 
+# be different in your installation.
 
 export CGO_LDFLAGS="-L$SGE_ROOT/lib/lx-amd64/"
 export CGO_CFLAGS="-I$SGE_ROOT/include"
